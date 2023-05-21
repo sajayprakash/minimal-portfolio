e9bf354fb38abad1d25362e85982088be70d7bf8
sajayprakash <sajayprakash7@protonmail.com>
2023-05-21 10:53:15 +0530

    fix contact-form endpoint

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,7 +5,7 @@ export default function Contact() {
     <div className="flex flex-col mb-10 mx-auto">
       <div className="flex justify-center items-center">
         <form
-          action="https://getform.io/f/140c54ba-df1c-42b7-85c0-4f5bc7bbb270"
+          action="https://getform.io/f/c2c686fe-24bb-44e4-9bb0-3611899d2aa1"
           method="POST"
           className="flex flex-col w-full md:w-7/12"
         >
@@ -29,7 +29,6 @@ export default function Contact() {
             className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"
           />
           <button
-            type="button"
             className="text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-purple-500 to-violet-500 drop-shadow-md hover:from-purple-600 hover:to-violet-600"
           >
             Contact Me
